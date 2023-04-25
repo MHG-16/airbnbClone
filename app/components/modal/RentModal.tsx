f7e7313892810c42d9a05057d4e0fbff90b92a32
mhg-16 <hammahadi2006@gmail.com>
2023-04-25 22:41:22 +0200

    Info step

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -11,6 +11,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../Input/CountrySelect';
 import Map from '../share/Map';
 import dynamic from 'next/dynamic';
+import Counter from '../Input/Counter';
 
 
 enum STEPS {
@@ -42,6 +43,7 @@ const RentModal = () => {
       location: null,
       guestCount: 1,
       roomCount: 1,
+      bathroomsCount: 1,
       imgSrc: '',
       description: '',
       price: 1,
@@ -51,6 +53,9 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomsCount = watch('bathroomsCount')
 
   const Map = useMemo(() => dynamic(() => import ('../share/Map'), {
     ssr: false
@@ -127,6 +132,24 @@ const RentModal = () => {
         <Heading 
           title={"Share some basics about your place"}
           subtitle={"What amenities do you have?"}
+        />
+        <Counter 
+          title='Guests'
+          subtitle='How many guests do you allow?'
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <Counter 
+          title='Rooms'
+          subtitle='How many rooms do you have?'
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <Counter 
+          title='Bathrooms'
+          subtitle='How many bathrooms do you have?'
+          value={bathroomsCount}
+          onChange={(value) => setCustomValue('bathroomsCount', value)}
         />
       </div>
     );
